4a28fdaaa9791e8284bfd5a85dc040b90143855a
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-05-24 19:21:25 +0530

    new

--- newsapp/src/components/NewsItems.js
+++ newsapp/src/components/NewsItems.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react'
 
 export class NewsItems extends Component {
+
   render() {
-    let {title,description,imageurl}=this.props;
+
+    let {
+      title,
+      description,
+      imageurl,
+      newsurl
+    } = this.props;
+
     return (
-      
+
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-  <img src={imageurl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+
+        <div className="card" style={{ width: "18rem" }}>
+
+          <img
+            src={
+              imageurl
+                ? imageurl
+                : "https://via.placeholder.com/300"
+            }
+            className="card-img-top"
+            alt="news"
+          />
+
+          <div className="card-body">
+
+            <h5 className="card-title">
+              {title ? title.slice(0, 45) : ""}
+            </h5>
+
+            <p className="card-text">
+              {description
+                ? description.slice(0, 88)
+                : ""}
+            </p>
+
+            <a
+              href={newsurl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+            >
+              Read More
+            </a>
+
+          </div>
+        </div>
+
       </div>
-      
     )
   }
 }
