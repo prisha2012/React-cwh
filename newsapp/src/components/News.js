135994986be0c10faa7e31f29b4ebac2d829b79c
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-06-20 01:29:25 +0530

    new

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,40 +1,54 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
- 
-  constructor(){
+  constructor() {
     super();
-    this.state={
+
+    this.state = {
       articles: [],
-      loading: false
-    }
+      loading: false,
+      page: 1,
+    };
   }
-  async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=516e919071dc46b58db77d7ba6660fa3";
-    let data=await fetch(url);
-    let parsedData=await data.json();
-    this.setState({articles: parsedData.articles})
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=YOUR_API_KEY&page=1";
+
+    let response = await fetch(url);
+    let parsedData = await response.json();
+
+    this.setState({
+      articles: parsedData.articles,
+    });
   }
+
   render() {
     return (
-      <div className='container my-3'>
+      <div className="container my-3">
         <h2>News Monkey</h2>
 
-        <div className='row'>
-         {this.state.articles.map((element)=>{
-          return <div className='col-md-3' key={element.url} >
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3" key={element.url}>
+                <NewsItems
+                  title={element.title?.slice(0, 44)}
+                  description={element.description?.slice(0, 88)}
+                  imageurl={element.urlToImage}
+                  newsurl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <button className="btn btn-primary" type="submit">Button</button>
+        <button className="btn btn-primary" type="submit">Button</button>
 
-        <NewsItems title={element.title.slice(0,44)} description={element.description.slice(0,88)} imageurl={element.urlToImage} newsurl={element.url}/>
-        </div>
-        })}
-        
-       
-        </div>
-        
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
